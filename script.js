d3b188a8a7114645048554a37eedb01b8f029982
Sami9-0 <samsontsegenate@gmail.com>
2022-12-19 18:47:24 +0300

    Added operator function that takes two numbers and an operation and performs and the required operatio

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-
-
-
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
+let operate = (a, operation, b) => {
+    if (operation === '+')
+    {
+        return add(a, b);
+    }
+    else if (operation === '-')
+    {
+        return subtract(a, b);
+    }
+    else if (operation === 'x')
+    {
+        return multiply(a, b);
+    }
+    else if (operation === '/')
+    {
+        return divide(a, b);
+    }
+    else{
+        console.log('wrong');
+    }
+};
