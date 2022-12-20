da58f44dba389ff0190c75a957c48cc2fc30f6ef
Sami9-0 <samsontsegenate@gmail.com>
2022-12-20 22:10:21 +0300

    Got inputs from user via html buttons Can make and display calculation Clear button works Backcpace button doesn't work

--- script.js
+++ script.js
@@ -15,11 +15,70 @@ let operate = (a, operation, b) => {
     {
         return multiply(a, b);
     }
-    else if (operation === '/')
+    else if (operation === '÷')
     {
         return divide(a, b);
     }
-    else{
-        console.log('wrong');
-    }
 };
+let makeCalc = () =>
+{
+    let firstNumberCon = parseInt(firstNumber);
+    let secondNumberCon = parseInt(secondNumber);
+    return operate(firstNumberCon, operation, secondNumberCon);
+}
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operation');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const backSpace = document.querySelector('.back-space');
+const bigDisplay = document.querySelector('.big-display');
+const minDisplay = document.querySelector('.min-display');
+let firstNumber = '';
+let secondNumber = '';
+let operation = '';
+//To switch input from firstNumber to secondNumber
+let operationClicked = false;
+
+
+numbers.forEach(number => number.addEventListener('click', () => {
+    if (!operationClicked) {
+        firstNumber = firstNumber + number.textContent;
+        bigDisplay.textContent = firstNumber;
+    }
+    else
+    {
+        secondNumber = secondNumber + number.textContent;
+        bigDisplay.textContent = `${firstNumber} ${operation} ${secondNumber}`;
+    }
+}));
+operations.forEach(operations => operations.addEventListener('click', () => {
+    operation = operations.textContent;
+    operationClicked = true;
+    bigDisplay.textContent = `${firstNumber} ${operation}_`;
+}));
+equal.addEventListener('click', () => {
+    bigDisplay.textContent = makeCalc();
+    minDisplay.textContent = `${firstNumber} ${operation} ${secondNumber}`;
+    firstNumber = makeCalc();
+    secondNumber = '';
+});
+clear.addEventListener('click', () => {
+    operationClicked = false;
+    firstNumber = '';
+    secondNumber = '';
+    bigDisplay.textContent = '';
+    minDisplay.textContent = '';
+})
+backSpace.addEventListener('click', () => {
+    let text = bigDisplay.textContent;
+    if (charAt(text.length - 1) === '_')
+    {
+        bigDisplay.textContent = text.slice(0, -3);
+        operationClicked = false;
+    }
+    else
+    {
+        bigDisplay.textContent = text.replace(/ .$/, '');
+        operationClicked = false;
+    }
+});
